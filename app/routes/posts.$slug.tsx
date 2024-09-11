7eb4ba90ec80bdd419103a8a0db691a047ab301c
Arisa Miyashita <miyashitaarisa@gmail.com>
2024-09-11 12:11:43 +0900

    動的ルートでpostの内容を取得で切るようになりました。

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,10 +1,48 @@
-export default function PostSlug() {
-    return (
-      <main className="mx-auto max-w-4xl">
-        <h1 className="my-6 border-b-2 text-center text-3xl">
-          Some Post
-        </h1>
-      </main>
-    );
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+interface Post {
+  slug: string;
+  title: string;
+  markdown: string;
+}
+
+export async function getPost(slug: string, posts: Post[]) 
+{
+  return posts.find((post) => post.slug === slug);
+}
+
+export const loader = async ({ params, context }: LoaderFunctionArgs) => {
+  const { slug } = params;
+
+  if (!slug) {
+    throw new Response("Post not found", { status: 404 });
   }
-  
+
+  const posts = await context.db.post.findMany();
+
+  const post = await getPost(slug, posts);
+
+  if (!post) {
+    throw new Response("Post not found", { status: 404 });
+  }
+
+  return json({ post });
+};
+
+export default function PostSlug() {
+    
+  const { post } = useLoaderData<typeof loader>();
+
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">
+        {post.title}
+      </h1>
+      <article className="prose">
+        <p>{post.markdown}</p>
+      </article>
+    </main>
+  );
+}
