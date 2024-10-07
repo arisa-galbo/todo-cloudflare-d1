c08b4e11dddd76641eca9e9bd0c2551cabcce798
Arisa Miyashita <miyashitaarisa@gmail.com>
2024-10-07 13:36:03 +0900

    パスを相対パスに変更しました

--- app/routes/posts.admin._index.tsx
+++ app/routes/posts.admin._index.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@remix-run/react";
 import "../styles/admin.css";
 import type { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticator } from "~/services/auth.server";
+import { authenticator } from "../services/auth.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   return await authenticator.isAuthenticated(request, {
